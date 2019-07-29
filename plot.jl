5448d53bbb71965338f4362a945ffd79c0b945d7
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2019-07-29 20:56:08 +0200

    Wykresy okien.

--- plot.jl
+++ plot.jl
@@ -2,10 +2,20 @@ using IOLA
 using PGFPlotsX
 PGFPlotsX.enable_interactive(false)
 
-par = Codec.getParams(Codec.MP3)
+axis_theme = @pgf {xmajorgrids,
+                   ymajorgrids,
+                   "/pgf/number format/use comma",
+                   "/pgf/number format/1000 sep"={},
+                   "every axis plot/.style"={no_marks, thick}
+                  };
 
-win = par.window(par.length)
+for codec in instances(Codec.CodecType)
+    par = Codec.getParams(codec)
 
-fig = @pgf TikzPicture(Axis(Plot(Table(; x = 1:par.length, y = win))))
+    win = par.window(par.length)
 
-pgfsave("plots/win_sine.pgf", fig)
+    fig = @pgf TikzPicture(Axis({axis_theme..., xlabel = raw"$n$", ylabel = raw"$w(n)$"},
+                                Plot(Table(; x = 1:par.length, y = win))))
+
+    pgfsave("plots/win_$codec.pgf", fig)
+end
