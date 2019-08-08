f58a57c77d0b61fd8df27a5e0b336bd0f6f0da88
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2019-08-08 23:41:19 +0200

    Więcej o śmolomo

--- plot.jl
+++ plot.jl
@@ -1,5 +1,9 @@
 using IOLA
+using JLD2, FileIO
 using PGFPlotsX
+using IOLA.Utils: fast_sum_abs_log10_abs!
+using BenchmarkTools
+using Statistics
 PGFPlotsX.enable_interactive(false)
 
 axis_theme = @pgf {xmajorgrids,
@@ -9,13 +13,57 @@ axis_theme = @pgf {xmajorgrids,
                    "every axis plot/.style"={no_marks, thick}
                   };
 
-for codec in instances(Codec.CodecType)
-    par = Codec.getParams(codec)
+function plot_windows()
+    for codec in instances(Codec.CodecType)
+        par = Codec.getParams(codec)
 
-    win = par.window(par.length)
+        win = par.window(par.length)
 
-    fig = @pgf TikzPicture(Axis({axis_theme..., xlabel = raw"$n$", ylabel = raw"$w(n)$"},
-                                Plot(Table(; x = 1:par.length, y = win))))
+        fig = @pgf TikzPicture(Axis({axis_theme..., xlabel = raw"$n$", ylabel = raw"$w(n)$"},
+                                    Plot(Table(; x = 1:par.length, y = win))))
 
-    pgfsave("plots/win_$codec.pgf", fig)
+        pgfsave("plots/win_$codec.pgf", fig)
+    end
+end
+
+function plot_sum_abs_log_abs(recalc=false)
+
+    data = if recalc
+        suite = BenchmarkGroup()
+        suite["naive"] = BenchmarkGroup()
+        suite["IOLA"] = BenchmarkGroup()
+
+        function sum_abs_log10_abs(x)
+            sum(abs.(log10.(abs.(x))))
+        end
+
+        sizes = 10 .^ (0:7)
+        for size in sizes
+            data = rand(size)
+            suite["naive"][size] = @benchmarkable sum_abs_log10_abs($data)
+            suite["IOLA"][size] = @benchmarkable fast_sum_abs_log10_abs!(copy($data))
+        end
+
+        tune!(suite)
+        results = run(suite)
+
+        res = Vector()
+
+        for size in sizes
+            push!(res, median(results["naive"][size].times)/median(results["IOLA"][size].times))
+        end
+        sum_abs_log_abs = Dict("x" => sizes, "y" => res)
+        @save "plot_data.jld2" sum_abs_log_abs
+        sum_abs_log_abs
+    else
+        @load "plot_data.jld2" sum_abs_log_abs
+        sum_abs_log_abs
+    end
+
+    fig = @pgf TikzPicture(SemiLogXAxis({axis_theme..., xlabel=raw"$K$",
+                                         ylabel=raw"Stosunek czasu wykonywania",
+                                         ytick_distance=0.5, xtick=data["x"]},
+                                        Plot(Table(; x=data["x"], y=data["y"]))))
+
+    pgfsave("plots/sum_abs_log10_abs.pgf", fig)
 end
